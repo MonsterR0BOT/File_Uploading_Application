b4f63dd3d07d0931025ed6c669ea0a29ecce7a6b
Shovan Mishra <ishovanmishra@gmail.com>
2021-05-16 12:57:18 +0530

    first commit

---
+++ resources/views/admin_tables/userstable.blade.php
@@ -0,0 +1,34 @@
+<table class="table table-striped table-bordered zero-configuration">
+    <thead>
+        <tr>
+            <th>SN. No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Company ID</th>
+            <th>Created at</th>
+            <th>Change Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($getusers as $users) {
+        ?>
+        <tr id="dataid{{$users->id}}">
+            <td>{{$users->id}}</td>
+            <td>{{$users->name}}</td>
+            <td>{{$users->email}}</td>
+            <td>{{$users->Company_id}}</td>
+            <td>{{$users->created_at}}</td>
+            <td>
+                @if($users->role_id == '2')
+                    <a class="badge badge-info px-2" onclick="StatusUpdate('{{$users->id}}','1')" style="color: #fff;">User</a>
+                @else
+                    <a class="badge badge-primary px-2" onclick="StatusUpdate('{{$users->id}}','2')" style="color: #fff;">Admin</a>
+                @endif
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
